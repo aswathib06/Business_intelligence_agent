47afa5ea60a7ff1f8d379645de551b6ef50befeb
aswathib06 <maluaswathi985@gmail.com>
2026-03-01 17:53:31 +0530

    Refactor data loading to use local file paths

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,13 @@
 import pandas as pd
-from config import DEALS_PATH, WORK_ORDERS_PATH
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def load_data():
-    deals = pd.read_excel(DEALS_PATH)
-    work_orders = pd.read_excel(WORK_ORDERS_PATH)
-    return deals, work_orders
+    deals_path = os.path.join(BASE_DIR, "Deal funnel Data.xlsx")
+    work_orders_path = os.path.join(BASE_DIR, "Work_Order_Tracker Data.xlsx")
+
+    deals = pd.read_excel(deals_path)
+    work_orders = pd.read_excel(work_orders_path)
+
+    return deals, work_orders deals, work_orders
